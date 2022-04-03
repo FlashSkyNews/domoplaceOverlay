cc96409daa69d8fea54f002a5ae69376d3bed2a7
Kayo <24974529+FlashSkyNews@users.noreply.github.com>
2022-04-04 00:12:33 +0200

    Create domoplaceoverlay.user.js

---
+++ domoplaceoverlay.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         r/papaplatte
+// @namespace    http://tampermonkey.net/
+// @version      1
+// @description  try to take over the canvas!
+// @author       mp1404
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @updateURL    https://github.com/FlashSkyNews/domoplaceOverlay/raw/main/domoplaceoverlay.user.js
+// @downloadURL  https://github.com/mp14O4/domoplaceOverlay/raw/main/domoplaceoverlay.user.js
+// ==/UserScript==
+const url = "https://raw.githubusercontent.com/FlashSkyNews/domoPlace/main/overlay.png"
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+            (function () {
+                const i = document.createElement("img");
+                i.src = url;
+                i.onload = () => {
+                    if (i.width === i.height) {
+                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+                    } else {
+                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+                    }
+                };
+                return i;
+            })())
+    }, false);
+}
