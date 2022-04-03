e4a247288da546d2f1457589baa24ee682207056
Kayo <24974529+FlashSkyNews@users.noreply.github.com>
2022-04-04 00:49:29 +0200

    Update domoplaceoverlay.user.js

--- domoplaceoverlay.user.js
+++ domoplaceoverlay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/papaplatte
 // @namespace    http://tampermonkey.net/
-// @version      1
+// @version      2
 // @description  try to take over the canvas!
 // @author       mp1404
 // @match        https://hot-potato.reddit.com/embed*
@@ -9,20 +9,14 @@
 // @updateURL    https://github.com/FlashSkyNews/domoplaceOverlay/raw/main/domoplaceoverlay.user.js
 // @downloadURL  https://github.com/FlashSkyNews/domoplaceOverlay/raw/main/domoplaceoverlay.user.js
 // ==/UserScript==
-const url = "https://raw.githubusercontent.com/FlashSkyNews/domoPlace/main/overlay.png"
+let url = "https://raw.githubusercontent.com/FlashSkyNews/domoPlace/main/overlay.png"
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
         document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
             (function () {
                 const i = document.createElement("img");
                 i.src = url;
-                i.onload = () => {
-                    if (i.width === i.height) {
-                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
-                    } else {
-                        i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
-                    }
-                };
+                i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 2000px;";
                 return i;
             })())
     }, false);
